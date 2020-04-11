a7d26abd62ae87c4f6c893be1c0588a7e4229f7c
pavankishore613 <63142909+pavankishore613@users.noreply.github.com>
2020-04-11 14:14:10 +0530

    updated knn
    brought 2000 data to 20000

--- KNN-Outlier.py
+++ KNN-Outlier.py
@@ -11,16 +11,16 @@ data = pd.read_csv("creditcard.csv")
 print(data.shape)
 print(data.head())
 print(data["Class"].value_counts())
-data_20000 = data[:2000]
+data_20000 = data[:20000]
 data20000 = data_20000.drop(['Class'], axis=1)
 data20000_labels = data_20000["Class"]
 data20000_Std = StandardScaler().fit_transform(data20000)
 print(data20000_Std.shape)
 print(type(data20000_Std))
 X_train = data20000_Std[0:16000,1:2]
-X_test = data20000_Std[16000:2000, 1:2]
+X_test = data20000_Std[16000:20000, 1:2]
 y_train = data20000_labels[0:16000]
-y_test = data20000_labels[16000:2000]
+y_test = data20000_labels[16000:20000]
 #let's plot our train and test set
 knn=KNN(contamination=outlier_fraction)
 knn.fit(X_train)
